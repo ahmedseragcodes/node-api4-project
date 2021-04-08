c578e3cc0a8f3ae07e74d4423e2e7a236a58d361
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-08 18:36:50 -0400

    added script for flow for heroku to run post build to deploy front end

--- index.js
+++ index.js
@@ -5,20 +5,28 @@ console.log("WEB 40 RULES");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 
 //INSTANCE OF THE SERVER
 const server = express();
 
 //GLOBAL MIDDLEWARE
 
+server.use(express.json());
+server.use(express.static(path.join(__dirname, "client/build")))
+
 if (process.env.NODE_ENV === "development"){
-    server.use(express.json());
     server.use(cors());
 }
 
+//our API comes earlier in the pipeline
+server.get("/api/hello", (req, res)=>{
+    res.json({message: "hello"});
+})
 
-server.use("*", (req, res)=>{
-    res.send("<h1>Success</h1>")
+//catch-all sends back index.html
+server.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
 })
 
 
