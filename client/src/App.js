6fe74214f69bcdad0fbce01b8c8f97727c901963
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-08 19:08:33 -0400

    setup react app and fetch to begin bringing in data from the api endpoint i constructed

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
 
 function App() {
+const [message, setMessage]=useState("Hello Web 40");
+
+
+useEffect(()=>{
+  fetch("http://localhost:5000/api/hello")
+  .then(res=>
+    res.json())
+  .then(resBody=>
+    setMessage(resBody.message))
+  .catch(err=>{
+    console.log("UNABLE TO GET MESSAGE", err);
+  })
+},[])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {message}
       </header>
     </div>
   );
