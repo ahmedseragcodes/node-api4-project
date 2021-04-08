39fe3c9ebf914b644ca16de0b31b34bc406823cc
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-08 19:18:58 -0400

    reformatted useEffect for populating state via api with another method as a trial

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,29 @@ function App() {
 const [message, setMessage]=useState("Hello Web 40");
 
 
+// useEffect(()=>{
+//   fetch("http://localhost:5000/api/hello")
+//   .then(res=>
+//     res.json())
+//   .then(resBody=>
+//     setMessage(resBody.message))
+//   .catch(err=>{
+//     console.log("UNABLE TO GET MESSAGE", err);
+//   })
+// },[])
+
+//OR WRITE THAT USE EFFECT LIKE THIS
+
+function getURL(path){
+  if (process.env.NODE_ENV==="development"){
+    return "http://localhost:5000" + path
+  } else {
+    return path
+  }
+}
+
 useEffect(()=>{
-  fetch("http://localhost:5000/api/hello")
+  fetch(getURL("/api/hello"))
   .then(res=>
     res.json())
   .then(resBody=>
@@ -19,6 +40,8 @@ useEffect(()=>{
     console.log("UNABLE TO GET MESSAGE", err);
   })
 },[])
+
+//END OR 
 
   return (
     <div className="App">
