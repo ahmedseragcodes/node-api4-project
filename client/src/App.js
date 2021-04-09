4aa4b35d32297ebb0919f3c5b5241fa153f32cfd
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-08 21:28:02 -0400

    wrote a couple more methods for populating state with API data for practice / education

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
@@ -9,39 +8,61 @@ function App() {
 const [message, setMessage]=useState("Hello Web 40");
 
 
+
+//LOGIC FOR POPULATING STATE WITH API DATA FROM ENDPOINT CREATED ON BACKEND
+
+//METHOD 1 - SUCCESSFUL 
+
+useEffect(()=>{
+  fetch("http://localhost:5000/api/hello")
+  .then((res)=>{
+    return res.json();
+  })
+  .then((resBody)=>{
+    setMessage(resBody.message);
+  })
+  .catch((err)=>{
+    console.log("UNABLE TO GET MESSAGE", err);
+  })
+},[])
+
+//METHOD 2 - SUCCESSFUL 
+
+// function getURL(path){
+//   if (process.env.NODE_ENV==="development"){
+//     return "http://localhost:5000" + path
+//   } else {
+//     return path
+//   }
+// }
+
 // useEffect(()=>{
-//   fetch("http://localhost:5000/api/hello")
-//   .then(res=>
-//     res.json())
-//   .then(resBody=>
-//     setMessage(resBody.message))
+//   fetch(getURL("/api/hello"))
+//   .then(res=> res.json())
+//   .then(resBody=>setMessage(resBody.message))
 //   .catch(err=>{
 //     console.log("UNABLE TO GET MESSAGE", err);
 //   })
 // },[])
 
-//OR WRITE THAT USE EFFECT LIKE THIS
+//METHOD 3 - SUCCESSFUL 
 
-function getURL(path){
-  if (process.env.NODE_ENV==="development"){
-    return "http://localhost:5000" + path
-  } else {
-    return path
-  }
-}
+// useEffect(()=>{
+//   axios.get("http://localhost:5000/api/hello")
+//   .then((res)=>{
+//     console.log("THIS IS WHAT THE API DATA LOOKS LIKE", res);
+//     setMessage(res.data.message);
+//   })
+//   .catch((err)=>{
+//     console.log("FAILED TO GET API DATA", err);
+//   })
+// },[])
 
-useEffect(()=>{
-  fetch(getURL("/api/hello"))
-  .then(res=>
-    res.json())
-  .then(resBody=>
-    setMessage(resBody.message))
-  .catch(err=>{
-    console.log("UNABLE TO GET MESSAGE", err);
-  })
-},[])
 
-//END OR 
+//END LOGIC FOR POPULATING STATE WITH BACKEND API DATA
+
+
+//BEGIN FUNCTION COMPONENT RETURN 
 
   return (
     <div className="App">
